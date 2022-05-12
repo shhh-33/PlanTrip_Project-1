3845af900dd5b1bad40e11492b935322b1d4f79b
sh <lop9036@naver.com>
2022-05-12 20:51:04 +0900

    Revert "여행 만들기 관련 로직 생성"
    This reverts commit 08c99147830079ebcfe838649b49254bfaabd5bc.

--- plantrip/src/main/java/com/plantrip/controller/mainController.java
+++ plantrip/src/main/java/com/plantrip/controller/mainController.java
@@ -2,11 +2,12 @@ package com.plantrip.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-public class MainController {
+public class mainController {
 
     @RequestMapping("/")
     public String main(){
