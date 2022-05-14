3a34c709bed3c2e1a8adb9fc53c38269186f5f64
Younsun Choi <younsun0107@gmail.com>
2022-05-14 10:48:56 +0900

    Register, 회원가입 / userformDto, user Entity, userRepository, userService

--- plantrip/src/main/java/com/plantrip/dto/UserFormDto.java
+++ plantrip/src/main/java/com/plantrip/dto/UserFormDto.java
@@ -1,17 +1,33 @@
 package com.plantrip.dto;
 
+
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
-@Getter @Setter
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
 public class UserFormDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
+    @NotEmpty(message="이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
+    @NotEmpty(message = "전화번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$"
+            , message = "전화번호는 최소 10자리, 최대 11 자리의 숫자만 입력 가능합니다.")
     private String phone;
 
 }
--- plantrip/src/main/java/com/plantrip/repository/UserRepository.java
+++ plantrip/src/main/java/com/plantrip/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.plantrip.repository;
 import com.plantrip.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+    //회원 가입시 중복된 회원이 있는지 검사하기 위해서 이메일로 회원을 검사할 수 있도록 쿼리 메소드를 작성합니다.
+
 
 }
